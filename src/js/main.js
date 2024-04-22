c0533ae05eb27c2d931b1b6a204b1e3c4be64a64
Camilobarre <barrecami@gmail.com>
2024-04-22 10:37:32 -0500

    Se crea la semática del HTML y se inicia con la clase

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+const listFruits = [
+    "apple",
+    "orange",
+    "banana",
+    "grape",
+    "strawberry",
+    "kiwi",
+    "pineapple",
+    "watermelon",
+    "mango",
+    "pear",
+    "peach",
+    "plum",
+    "cherry",
+    "blueberry",
+    "raspberry",
+    "blackberry",
+    "lemon",
+    "lime",
+    "coconut",
+    "pomegranate"
+  ];
+
+  // Print list with classic for
+
+  
